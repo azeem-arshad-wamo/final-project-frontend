6d356c002ed5405e67e874cbef3b77f5d0070c54
Azeem Arshad Khokhar <azeem.arshad@wamolabs.com>
2026-01-26 23:32:33 +0500

    Feature(SignUp): Implemented Login for signup page

--- src/components/signup-form.jsx
+++ src/components/signup-form.jsx
@@ -9,31 +9,102 @@ import {
   FieldSeparator,
 } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
 export function SignupForm({ className, ...props }) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [error, setError] = useState(null);
+
+  const LoginSchema = Yup.object({
+    firstName: Yup.string()
+      .required("First name is required")
+      .min(2, "First name cannot be smaller than 2 characters")
+      .max(20, "First name cannot be longer than 20 characters"),
+    lastName: Yup.string()
+      .required("Last name is required")
+      .min(2, "Last name cannot be smaller than 2 characters")
+      .max(20, "Last name cannot be longer than 20 characters"),
+    email: Yup.string().email().required("Email is required"),
+    password: Yup.string()
+      .min(8, "Password must be at least 8 characters")
+      .max(20, "Password cannot be longer than 20 characters")
+      .required("Password is required"),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+    },
+    validationSchema: LoginSchema,
+    onSubmit: async (values, { setSubmitting }) => {
+      setError(null);
+      setSubmitting(true);
+      try {
+        const formData = {
+          firstName: values.firstName,
+          lastName: values.lastName,
+          email: values.email,
+          password: values.password,
+        };
+        console.log(formData);
+        // await dispatch().unwrap();
+        // navigate("/");
+      } catch (error) {
+        console.log(error);
+        console.log("SETTING ERROR STATE");
+        setError(error);
+      } finally {
+        setSubmitting(false);
+      }
+    },
+  });
+
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card className="overflow-hidden p-0">
         <CardContent className="grid p-0 md:grid-cols-2">
-          <form className="p-6 md:p-8">
+          <form className="p-6 md:p-8" onSubmit={formik.handleSubmit}>
             <FieldGroup>
               <div className="flex flex-col items-center gap-2 text-center">
                 <h1 className="text-2xl font-bold">Create your account</h1>
                 <p className="text-muted-foreground text-sm text-balance">
                   Enter your email below to create your account
                 </p>
+                {error && <p className="text-red-500 text-sm mt-1">{error}</p>}
               </div>
               <Field>
                 <Field className="grid grid-cols-2 gap-4">
                   <Field>
                     <FieldLabel htmlFor="firstName">First Name</FieldLabel>
-                    <Input id="firstName" type="text" required />
+                    <Input
+                      id="firstName"
+                      type="text"
+                      required
+                      name="firstName"
+                      values={formik.values.firstName}
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                    />
                   </Field>
                   <Field>
-                    <FieldLabel htmlFor="confirm-password">
-                      Last Name
-                    </FieldLabel>
-                    <Input id="lastName" type="text" required />
+                    <FieldLabel htmlFor="Last Name">Last Name</FieldLabel>
+                    <Input
+                      id="lastName"
+                      type="text"
+                      required
+                      name="lastName"
+                      values={formik.values.lastName}
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                    />
                   </Field>
                 </Field>
               </Field>
@@ -44,6 +115,10 @@ export function SignupForm({ className, ...props }) {
                   type="email"
                   placeholder="m@example.com"
                   required
+                  name="email"
+                  values={formik.values.email}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
                 />
               </Field>
               <Field>
@@ -53,6 +128,10 @@ export function SignupForm({ className, ...props }) {
                   type="password"
                   placeholder="********"
                   required
+                  name="password"
+                  values={formik.values.password}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
                 />
               </Field>
               <Field>
