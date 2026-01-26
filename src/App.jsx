9f17077e0e5bbdb17fa52a37fb1095effa7ff709
Azeem Arshad Khokhar <azeem.arshad@wamolabs.com>
2026-01-26 17:29:37 +0500

    feature(router): Added Home.jsx (Home Page) to browser Rotuer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { BrowserRouter, Route, Router } from "react-router-dom";
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import Home from "./features/Home/Home.jsx";
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
-        <Router>
-          <Route path="/" element={""} />
-        </Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </BrowserRouter>
     </>
   );
