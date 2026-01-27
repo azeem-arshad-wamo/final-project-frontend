b9b9aa08dafdb802b493494743d1ab7011fd7695
Azeem Arshad Khokhar <azeem.arshad@wamolabs.com>
2026-01-27 19:50:12 +0500

    Fix(SinglePost): Fixed displaying issue

--- src/features/Post/Post.jsx
+++ src/features/Post/Post.jsx
@@ -8,19 +8,63 @@ export default function Post() {
   const dispatch = useDispatch();
   const post = useSelector(selectCurrentSelectedPost);
 
-  console.log(`ID from params: ${id}`);
-  console.log(post);
-
   useEffect(() => {
     if (id) {
       dispatch(getPostById(id));
     }
   }, [id, dispatch]);
 
+  if (!post) {
+    return (
+      <>
+        <div className="flex items-center justify-center h-full">
+          <h1 className="text-3xl">Could not find the post</h1>
+        </div>
+      </>
+    );
+  }
+  console.log(`ID from params: ${id}`);
+  console.log(post.blocks);
+
   return (
     <>
-      <div>
-        <h1>Singe Post Page</h1>
+      <div className="min-h-screen flex flex-col items-center py-10 px-5">
+        <div className="w-full max-w-3xl flex flex-col gap-10">
+          <div className="flex flex-col gap-2">
+            <p className="text-5xl font-bold">{post.title}</p>
+            <p>Post ID: {post.id}</p>
+          </div>
+          <div>
+            {post.blocks && post.blocks.length > 0 ? (
+              <div>
+                {post.blocks.map((block, index) => {
+                  switch (block.type) {
+                    case "heading":
+                      return (
+                        <p className="text-4xl" key={index}>
+                          {block.data}
+                        </p>
+                      );
+                    case "sub-heading":
+                      return (
+                        <p className="text-2xl" key={index}>
+                          {block.data}
+                        </p>
+                      );
+                    case "text":
+                      return <p key={index}>{block.data}</p>;
+                    case "image":
+                      return <img key={index} src={block.data} alt="image" />;
+                    default:
+                      return null;
+                  }
+                })}
+              </div>
+            ) : (
+              <p className="text-3xl font-bold">No Blocks Found</p>
+            )}
+          </div>
+        </div>
       </div>
     </>
   );
